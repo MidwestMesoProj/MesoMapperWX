4b212f37fe873a70bdcafa0d13d4f11a2eb063c8
MidwestMesoProj <invincible102102@gmail.com>
2026-06-22 14:39:50 -0400

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error("Root div not found in index.html")
+}
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 )
