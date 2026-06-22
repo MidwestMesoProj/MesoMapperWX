1a252126d88eb206eb7ea9f1420db4b33b90d1fa
MidwestMesoProj <invincible102102@gmail.com>
2026-06-22 16:09:51 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from 'vite';
+import react from '@vitejs/react-refresh'; // or '@vitejs/plugin-react' depending on your file
 
 export default defineConfig({
   plugins: [react()],
-  base: '/MidwestMesoProj/', // <-- Tells GitHub Pages exactly where your repository lives
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './') // <-- Changed from './src' to './' since your files are in the root
-    }
-  }
-})
+  base: '/MesoMapperWX/', // <-- ADD THIS LINE RIGHT HERE
+});
