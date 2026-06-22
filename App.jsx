d1f1de8ae751af4456c2550a0f050fb4da0de506
MidwestMesoProj <invincible102102@gmail.com>
2026-06-22 14:32:44 -0400

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,30 +1,5 @@
-import { Toaster } from "@/components/ui/toaster"
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClientInstance } from '@/lib/query-client'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import PageNotFound from './lib/PageNotFound';
-import AppLayout from '@/components/weather/AppLayout';
-import Dashboard from '@/pages/Dashboard';
-import FormulaLibrary from '@/pages/FormulaLibrary';
-
 function App() {
-  return (
-    <QueryClientProvider client={queryClientInstance}>
-      <Router>
-        <Routes>
-          <Route element={<AppLayout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/library" element={<FormulaLibrary />} />
-          </Route>
-
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </Router>
-
-      <Toaster />
-    </QueryClientProvider>
-  );
+  return <h1>Hello World</h1>;
 }
 
 export default App;
